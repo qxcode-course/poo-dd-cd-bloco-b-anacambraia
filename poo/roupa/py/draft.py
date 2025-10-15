6c3dff264277cabcf87f0583bb6273ae2f9d9d7c
Ana Cambraia <anabeatrizsilvacambraia@gmail.com>
2025-10-15 14:34:42 +0000

    concluindo a camisa

--- poo/roupa/py/draft.py
+++ poo/roupa/py/draft.py
@@ -1,22 +1,32 @@
-class Camisa:
+class Roupa:
     def __init__(self):
         self.__tamanho = ""
 
     def getTamanho(self):
-        return self.__tamanho
+        return self.__tamanho 
 
     def setTamanho(self, valor):
         tamanhos_validos = ["PP", "P", "M", "G", "GG", "XG"]
         if valor in tamanhos_validos:
             self.__tamanho = valor
             return True
+        elif valor == "":
+            print("size: ()")
         else:
             print("Valor inválido! Os tamanhos permitidos são: PP, P, M, G, GG, XG.")
             return False
 
-camisa = Camisa()
-while camisa.getTamanho() == "": 
-    print("Digite seu tamanho de camisa")
-    tamanho = input() 
-    camisa.setTamanho(tamanho) 
-print("Parabens, você comprou uma camisa tamanho", camisa.getTamanho())
+    def __str__(self):
+        return f"{self.getTamanho}"
+
+def main():
+    roupa = Roupa ()
+    while True:
+        line = input()
+        print("$" + line)
+        args = line.split(" ")
+        if args[0] == "end":
+            break
+        elif args[0] == "show":
+            print(roupa)
+main()
