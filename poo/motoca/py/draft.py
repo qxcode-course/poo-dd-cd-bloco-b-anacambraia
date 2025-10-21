d1585e63b587ced0d0e60f7ec8cbb2b4772273bc
Ana Cambraia <anabeatrizsilvacambraia@gmail.com>
2025-10-21 19:52:57 +0000

    inciando a motoca

---
+++ poo/motoca/py/draft.py
@@ -0,0 +1,7 @@
+class Person:
+    def __init__(self, age : int, name : str):
+        self.age : int = age
+        self.name : str = name
+
+    def __str__(self):
+       return f"power:{this.power}, time:{this.time}, person:{this.person}"
